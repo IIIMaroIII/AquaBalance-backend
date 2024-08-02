82d5709f9c50c4503e4ca61cd01c37163b43ad70
Rodion Matvieiev <your.email@example.com>
2024-08-01 23:48:21 -0400

    base repository

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,12 +1,12 @@
-import { CLOUDINARY, COOKIE, USER } from '../constants/constants.js';
+// import { CLOUDINARY, COOKIE, USER } from '../constants/constants.js';
 import { Services } from '../services/index.js';
-import { GenerateCookie } from '../utils/GenerateCookie.js';
+// import { GenerateCookie } from '../utils/GenerateCookie.js';
 import { HttpError } from '../utils/HttpError.js';
-import { env } from '../utils/env.js';
-import { googleOauth } from '../utils/googleOauth.js';
+// import { env } from '../utils/env.js';
+// import { googleOauth } from '../utils/googleOauth.js';
 import { ResponseMaker } from '../utils/responseMaker.js';
-import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
-import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+// import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+// import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
 const RegisterController = async (req, res, next) => {
   const user = await Services.users.registerUser(req.body);
