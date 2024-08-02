667c112dd2aaf29a1ba312929aa726aec579d56a
InnaIvBoiko <inna_boiko@libero.it>
2024-08-02 12:27:27 +0200

    User registration, login, update, logout, token and session

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,12 +1,16 @@
-// import { CLOUDINARY, COOKIE, USER } from '../constants/constants.js';
+import {
+  CLOUDINARY,
+  COOKIE,
+  // USER
+} from '../constants/constants.js';
 import { Services } from '../services/index.js';
-// import { GenerateCookie } from '../utils/GenerateCookie.js';
+import { GenerateCookie } from '../utils/GenerateCookie.js';
 import { HttpError } from '../utils/HttpError.js';
-// import { env } from '../utils/env.js';
+import { env } from '../utils/env.js';
 // import { googleOauth } from '../utils/googleOauth.js';
 import { ResponseMaker } from '../utils/responseMaker.js';
-// import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
-// import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
 const RegisterController = async (req, res, next) => {
   const user = await Services.users.registerUser(req.body);
@@ -16,33 +20,127 @@ const RegisterController = async (req, res, next) => {
 };
 
 const LoginController = async (req, res, next) => {
-  // if (!session) return next(HttpError(500, 'Internal Server Error'));
-  // GenerateCookie(session, res);
+   const session = await Services.users.loginUser(req.body);
+  if (!session) return next(HttpError(500, 'Internal Server Error'));
+
+  GenerateCookie(session, res);
+
+  res.json(
+    ResponseMaker(200, 'You`ve been successfully logged in!', {
+      email: req.body.email,
+      accessToken: session.accessToken,
+    }),
+  );
 };
 
 const RefreshController = async (req, res, next) => {
-  // if (!sessionId || !refreshToken) {
-  //   return next(HttpError(401, 'Missing session cookies'));
-  // }
-  // if (!session) return next(HttpError(500, 'Internal Server Error', session));
-  // GenerateCookie(session, res);
+  const { sessionId, refreshToken } = req.cookies;
+
+  if (!sessionId || !refreshToken) {
+    return next(HttpError(401, 'Missing session cookies'));
+  }
+
+  const session = await Services.users.refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  if (!session) return next(HttpError(500, 'Internal Server Error', session));
+
+  GenerateCookie(session, res);
+
+  res.json(
+    ResponseMaker(200, 'The session has been successfully refreshed!', {
+      accessToken: session.accessToken,
+    }),
+  );
 };
 
-const UpdateController = async (req, res, next) => {};
+const UpdateController = async (req, res, next) => {
+  const { file, body } = req;
+  const { gender, name, email, weight, activeTime } = body;
+
+  let avatar;
+  let dailyNorma;
+
+  // dailyNorma = calculateDailyWaterIntake(
+  //   gender,
+  //   Number(weight),
+  //   Number(activeTime),
+  // );
+
+  if (file) {
+    if (env(CLOUDINARY.ENABLE_CLOUDINARY) === 'true') {
+      avatar = await saveFileToCloudinary(file);
+    } else {
+      avatar = await saveFileToUploadDir(file);
+    }
+  }
+  const result = await Services.users.updateUser(
+    { _id: req.user.id },
+    {
+      gender,
+      name,
+      email,
+      weight,
+      timeInSports: activeTime,
+      dailyNorma,
+      photoUrl: avatar,
+    },
+  );
+
+  if (!result) {
+    return next(
+      HttpError(404, `The contact with ${req.user._id} was not found!`),
+    );
+  }
+
+  res.json(
+    ResponseMaker(
+      200,
+      `Successfully updated a contact by id ${req.user._id}}!`,
+      result,
+    ),
+  );
+};
 
 const LogoutController = async (req, res, next) => {
-  // if (!sessionId || !refreshToken) {
-  //   return next(HttpError(401, 'Missing session cookies'));
-  // }
+  const { sessionId, refreshToken } = req.cookies;
+  console.log('sessionId', sessionId);
+  console.log('refreshToken', refreshToken);
+
+  if (!sessionId || !refreshToken) {
+    return next(HttpError(401, 'Missing session cookies'));
+  }
+
+  await Services.users.logoutUser({
+    sessionId: sessionId,
+    refreshToken: refreshToken,
+  });
+  res.clearCookie(COOKIE.SESSION_ID);
+  res.clearCookie(COOKIE.REFRESH_TOKEN);
+  res.status(204).send();
 };
 
-const RequestResetPasswordController = async (req, res) => {};
+const RequestResetPasswordController = async (req, res) => {
+  await Services.users.requestResetPassword(req.body.email);
+  res.json(
+    ResponseMaker(
+      200,
+      'The reset password email has been successfully sent!',
+      {},
+    ),
+  );
+};
 
-const ResetPwdController = async (req, res) => {};
+const ResetPwdController = async (req, res) => {
+  await Services.users.resetPwd(req.body);
+  res.json(ResponseMaker(200, 'The password has been successfully reset!', {}));
+};
 
-const getGoogleAuthUrlController = async (req, res) => {};
+// const getGoogleAuthUrlController = async (req, res) => {};
 
-const loginWithGoogleController = async (req, res) => {};
+// const loginWithGoogleController = async (req, res) => {};
 
 export const users = {
   RegisterController,
@@ -51,7 +149,7 @@ export const users = {
   LogoutController,
   RequestResetPasswordController,
   ResetPwdController,
-  getGoogleAuthUrlController,
-  loginWithGoogleController,
+  // getGoogleAuthUrlController,
+  // loginWithGoogleController,
   UpdateController,
 };
--- src/validation/authJoiSchema.js
+++ src/validation/authJoiSchema.js
@@ -1,22 +1,114 @@
 import joi from 'joi';
 
-const registerUserSchema = joi.object({});
+const registerUserSchema = joi.object({
+  email: joi
+    .string()
+    .trim()
+    .lowercase()
+    .email()
+    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .messages({
+      'string.email':
+        'Please fill a valid email address in lowercase. Examples of valid email addresses: john.doe@example.com, john-doe@example.com, john@example.co.uk,john.doe@example.co.in',
+      'any.required': 'Email address is required',
+    }),
+  password: joi.string().min(3).max(32).required().messages({
+    'any.required': 'Password is required!',
+  }),
+});
 
-const loginUserSchema = joi.object({});
+const loginUserSchema = joi.object({
+  email: joi
+    .string()
+    .trim()
+    .lowercase()
+    .email()
+    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .messages({
+      'string.email':
+        'Please fill a valid email address in lowercase. Examples of valid email addresses: john.doe@example.com, john-doe@example.com, john@example.co.uk,john.doe@example.co.in',
+      'any.required': 'Email address is required',
+    }),
+  password: joi.string().min(3).max(32).required().messages({
+    'any.required': 'Password is required!',
+  }),
+});
 
-const updateUserSchema = joi.object({});
+const updateUserSchema = joi.object({
+  name: joi.string().min(2).max(32).allow('').trim(),
+  gender: joi
+    .string()
+    .valid('woman', 'man')
+    .default('woman'),
+    // .required('Gender is required')
+    // .messages({
+    //   'any.required': 'Gender is required',
+    //   'string.valid': 'Invalid gender. Valid values are woman or man.',
+    // }),
+  waterIntake: joi
+    .number()
+    .allow('')
+    .positive('Value must be a positive number')
+    .precision(1)
+    .default(1.8),
+  weight: joi
+    .number()
+    .default(0),
+    // .positive()
+    // .required(),
+  photoUrl: joi.string().trim(),
+  activeTime: joi
+    .number()
+    .default(0),
+    // .allow(''),
+  email: joi
+    .string()
+    .allow('')
+    .trim()
+    .lowercase()
+    .email()
+    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    // .required()
+    .messages({
+      'string.email':
+        'Please fill a valid email address in lowercase. Examples of valid email addresses: john.doe@example.com, john-doe@example.com, john@example.co.uk,john.doe@example.co.in',
+      'any.required': 'Email address is required',
+    }),
+});
 
-const requestResetPasswordSchema = joi.object({});
+const requestResetPasswordSchema = joi.object({
+    email: joi
+    .string()
+    .trim()
+    .lowercase()
+    .email()
+    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .messages({
+      'string.email':
+        'Please fill a valid email address in lowercase. Examples of valid email addresses: john.doe@example.com, john-doe@example.com, john@example.co.uk,john.doe@example.co.in',
+      'any.required': 'Email address is required',
+    }),
+});
 
-const resetPwdSchema = joi.object({});
+const resetPwdSchema = joi.object({
+    token: joi.string().required().messages({
+    'any.required': 'Token is required!',
+  }),
+  password: joi.string().required().messages({
+    'any.required': 'Password is required!',
+  }),
+});
 
-const loginWithGoogleAuthSchema = joi.object({});
+// const loginWithGoogleAuthSchema = joi.object({});
 
 export const auth = {
   registerUserSchema,
   loginUserSchema,
   requestResetPasswordSchema,
   resetPwdSchema,
-  loginWithGoogleAuthSchema,
+  // loginWithGoogleAuthSchema,
   updateUserSchema,
 };
